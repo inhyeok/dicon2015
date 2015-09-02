e588075c0e96f7b8e61c4923c2bb67c647576266
Jeong Inhyeok <inhyeok0327@gmail.com>
2015-09-02 19:31:31 +0900

    validator fuck

--- public/js/dist/sign.js
+++ public/js/dist/sign.js
@@ -1,41 +1,54 @@
 (function() {
-  $(document).ready(function() {
+  jQuery(function() {
+    var options;
     $('.alertBox p').hide();
-    $('#u_email').focusout(function() {
-      var regex, u_email;
-      u_email = $('#u_email').val();
-      regex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/;
-      if (regex.test(u_email) === false) {
-        $('#email-alert p').show();
-        return false;
-      } else {
-        $('#email-alert p').hide();
-        return true;
+    this.email_alert = function() {
+      $('#email-alert p').show();
+      return false;
+    };
+    this.pw_alert = function() {
+      $('#pw-alert p').show();
+      return false;
+    };
+    this.pw_ck_alert = function() {
+      $('#pw-ck-alert p').show();
+      return false;
+    };
+    options = {
+      rules: {
+        u_name: {
+          required: true
+        },
+        u_email: {
+          required: true
+        },
+        u_pw: {
+          required: true,
+          minlength: 6,
+          maxlength: 12
+        },
+        u_pw_ck: {
+          equalTo: '#u_pw'
+        },
+        u_ph: {
+          required: true
+        }
+      },
+      massages: {
+        u_name: {
+          required: '이름을 입력해주세요.'
+        },
+        u_email: {
+          required: this.email_alert(),
+          email: this.email_alert()
+        },
+        u_pw: {
+          required: this.pw_alert()
+        }
       }
-    });
-    $('#u_pw').focusout(function() {
-      var u_pw;
-      u_pw = $('#u_pw').val();
-      if (u_pw.length < 6 || u_pw.length > 12) {
-        $('#pw-alert p').show();
-        return false;
-      } else {
-        $('#pw-alert p').hide();
-        return true;
-      }
-    });
-    return $('#u_pw_ck').focusout(function() {
-      var u_pw, u_pw_ck;
-      u_pw = $('#u_pw').val();
-      u_pw_ck = $('#u_pw_ck').val();
-      if (u_pw !== u_pw_ck) {
-        $('#pw-ck-alert p').show();
-        return false;
-      } else {
-        $('#pw-ck-alert p').hide();
-        return true;
-      }
-    });
+    };
+    $('#signForm').validate(options);
+    return true;
   });
 
 }).call(this);
--- public/js/sign.coffee
+++ public/js/sign.coffee
@@ -1,32 +1,41 @@
-$(document).ready( () ->
+jQuery ->
   $('.alertBox p').hide()
 
-  $('#u_email').focusout ->
-    u_email = $('#u_email').val()
-    regex = /^([\w-]+(?:\.[\w-]+)*)@((?:[\w-]+\.)*\w[\w-]{0,66})\.([a-z]{2,6}(?:\.[a-z]{2})?)$/
-    if regex.test(u_email) == false
-      $('#email-alert p').show()
-      return false
-    else
-      $('#email-alert p').hide()
-      return true
+  @email_alert = () ->
+    $('#email-alert p').show()
+    return false
 
-  $('#u_pw').focusout ->
-    u_pw = $('#u_pw').val()
-    if u_pw.length < 6 or u_pw.length >12
-      $('#pw-alert p').show()
-      return false
-    else
-      $('#pw-alert p').hide()
-      return true
+  @pw_alert = () ->
+    $('#pw-alert p').show()
+    return false
 
-  $('#u_pw_ck').focusout ->
-    u_pw = $('#u_pw').val()
-    u_pw_ck = $('#u_pw_ck').val()
-    if u_pw isnt u_pw_ck
-      $('#pw-ck-alert p').show()
-      return false
-    else
-      $('#pw-ck-alert p').hide()
-      return true
-)
+  @pw_ck_alert = () ->
+    $('#pw-ck-alert p').show()
+    return false
+
+  options =
+    rules:
+      u_name:
+        required: true
+      u_email:
+        required: true
+      u_pw:
+        required: true,
+        minlength: 6,
+        maxlength: 12
+      u_pw_ck:
+        equalTo: '#u_pw'
+      u_ph:
+        required: true
+    massages:
+      u_name:
+        required: '이름을 입력해주세요.'
+      u_email:
+        required: @email_alert(),
+        email: @email_alert()
+      u_pw:
+        required: @pw_alert()
+
+  $('#signForm').validate options
+
+  true
