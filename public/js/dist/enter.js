28bd5348f0aca883c92cf28b0df1d048253fc394
Jeong Inhyeok <inhyeok0327@gmail.com>
2015-09-03 16:42:32 +0900

    Change sign coffee to enter coffee

--- public/js/dist/enter.js
+++ public/js/dist/enter.js
@@ -1,44 +1,5 @@
 (function() {
   jQuery(function() {
-
-    /*
-    	options =
-    		rules:
-    			u_name:
-    				required: true
-    			u_email:
-    				required: true
-    			u_pw:
-    				required: true,
-    				minlength: 6,
-    				maxlength: 12
-    			u_pw_ck:
-    				equalTo: '#u_pw'
-    			u_ph:
-    				required: true,
-    				minlength: 9,
-    				maxlength: 11
-    		messages:
-    			u_name:
-    				required: '성명을 입력해주세요.'
-    			u_email:
-    				required: '이메일을 입력해주세요.',
-    				email: '옳바른 이메일을 입력해주세요.'
-    			u_pw:
-    				required: '비밀번호를 입력해주세요',
-    				minlength: '6글자이상 12글자 이하로 입력해주세요.',
-    				maxlength: '6글자이상 12글자 이하로 입력해주세요.'
-    			u_pw_ck:
-    				equalTo: '비밀번호가 일치하지 않습니다.'
-    			u_ph:
-    				required: '휴대번호를 입력해주세요.',
-    				minlength: '옳바른 번호를 입력해주세요.',
-    				maxlength: '옳바른 번호를 입력해주세요.'
-    
-    	$('#signForm').validate options
-    
-    	true
-     */
     var options;
     options = {
       framework: 'bootstrap',
@@ -99,7 +60,32 @@
         }
       }
     };
-    return $('#signForm').formValidation(options);
+    $('#signForm').formValidation(options);
+    options = {
+      framework: 'bootstrap',
+      icon: {
+        valid: 'glyphicon glyphicon-ok',
+        invalid: 'glyphicon glyphicon-remove',
+        validating: 'glyphicon glyphicon-refresh'
+      },
+      fields: {
+        l_email: {
+          validators: {
+            notEmpty: {
+              message: '항목값을 입력해주세요.'
+            }
+          }
+        },
+        l_pw: {
+          validators: {
+            notEmpty: {
+              message: '항목값을 입력해주세요.'
+            }
+          }
+        }
+      }
+    };
+    return $('#loginForm').formValidation(options);
   });
 
 }).call(this);
--- public/js/enter.coffee
+++ public/js/enter.coffee
@@ -1,42 +1,4 @@
 jQuery ->
-	###
-	options =
-		rules:
-			u_name:
-				required: true
-			u_email:
-				required: true
-			u_pw:
-				required: true,
-				minlength: 6,
-				maxlength: 12
-			u_pw_ck:
-				equalTo: '#u_pw'
-			u_ph:
-				required: true,
-				minlength: 9,
-				maxlength: 11
-		messages:
-			u_name:
-				required: '성명을 입력해주세요.'
-			u_email:
-				required: '이메일을 입력해주세요.',
-				email: '옳바른 이메일을 입력해주세요.'
-			u_pw:
-				required: '비밀번호를 입력해주세요',
-				minlength: '6글자이상 12글자 이하로 입력해주세요.',
-				maxlength: '6글자이상 12글자 이하로 입력해주세요.'
-			u_pw_ck:
-				equalTo: '비밀번호가 일치하지 않습니다.'
-			u_ph:
-				required: '휴대번호를 입력해주세요.',
-				minlength: '옳바른 번호를 입력해주세요.',
-				maxlength: '옳바른 번호를 입력해주세요.'
-
-	$('#signForm').validate options
-
-	true
-###
 
 	options =
 		framework: 'bootstrap'
@@ -79,3 +41,22 @@ jQuery ->
 						message: '정확한 번호를 입력해주세요.'
 
 	$('#signForm').formValidation options
+
+	options =
+		framework: 'bootstrap'
+		icon:
+			valid: 'glyphicon glyphicon-ok',
+			invalid: 'glyphicon glyphicon-remove',
+			validating: 'glyphicon glyphicon-refresh'
+		#excluded: ':disabled'
+		fields:
+			l_email:
+				validators:
+					notEmpty:
+						message: '항목값을 입력해주세요.'
+			l_pw:
+				validators:
+					notEmpty:
+						message: '항목값을 입력해주세요.'
+
+	$('#loginForm').formValidation options
